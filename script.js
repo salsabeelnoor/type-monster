07c4f93906871122839d20b155cf8a3beb44b82b
salsabeelnoor <salsabeelnoor@gmail.com>
2022-09-15 01:15:45 +0600

    time fixed

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ const typeController = (e) => {
     errorCount++;
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
   }
-  // console.log(errorCount);
 
   // check if given question text is equal to user typed text
   if (questionText === userText) {
@@ -69,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.floor((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -134,8 +133,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
-
+  const timeSpent = Math.floor((currentTime - startTime) / 1000);
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
